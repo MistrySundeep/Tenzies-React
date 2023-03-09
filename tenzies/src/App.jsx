cbc0dcfc093b6befce3d1070697ce630e5f5f4fa
Sundeep <mistrysundeep@gmail.com>
2023-03-09 14:48:39 +0000

    UI created and random numbers displayed on screen

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -1,34 +1,34 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import Die from './components/Dice'
 import './App.css'
 
-function App() {
+
+export default function App() {
   const [count, setCount] = useState(0)
 
+  function allNewDice(){
+    let numArr = []
+    for(let i = 0; i < 10; i++){
+      numArr.push(Math.floor(Math.random() * 6) + 1)
+    }
+    return numArr
+  }
+
+  let numbers = allNewDice()
+  console.log(numbers)
+
+  const dieItem = numbers.map((num) => {
+    return <Die 
+    value={num}
+    />
+  })
+
+
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <main>
+        <div onClick={allNewDice} className="dice-container">
+            {dieItem}
+        </div>
+    </main>
   )
 }
-
-export default App
